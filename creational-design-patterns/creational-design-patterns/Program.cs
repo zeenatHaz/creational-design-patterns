90a57ad66f28a50dfeb686b1e2c9a335deb080f3
Zeenat H <zeena@LAPTOP-D1AN8IIS>
2022-02-15 14:56:01 -0800

    added singleTon file

---
+++ creational-design-patterns/creational-design-patterns/Program.cs
@@ -0,0 +1,27 @@
+﻿using creational_design_patterns;
+
+namespace creationalDesignPattern{
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //parallel invocation of two methods.
+            Parallel.Invoke(
+                () => Print1stInvokation(),
+                () => Print2ndInvokation()
+                );
+            Console.ReadLine();
+        }
+        private static void Print1stInvokation()
+        {
+            Singleton obj = Singleton.GetInstance;
+            obj.PrintDetails("From 1 call");
+        }
+        private static void Print2ndInvokation()
+        {
+            Singleton obj = Singleton.GetInstance;
+            obj.PrintDetails("From 2 call");
+        }
+    }
+    }
---
+++ creational-design-patterns/creational-design-patterns/Singleton.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace creational_design_patterns
+{
+    public sealed class Singleton
+    {
+        private static Singleton GetSingleton;
+        private static int counter = 0;
+        public static Singleton Instance;
+
+        private static readonly Lazy<Singleton> Instancelock = new Lazy<Singleton>(() => new Singleton());
+        private Singleton()
+        {
+            counter++;
+            Console.WriteLine("Counter Value " + counter.ToString());
+        }
+        public static Singleton GetInstance
+        {
+            get
+            {
+                return Instancelock.Value;
+            }
+        }
+
+        public void PrintDetails(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
